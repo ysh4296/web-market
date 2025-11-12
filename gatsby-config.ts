86c93a16081fc58e04b1b24b2c4966acfbd7b2a3
cadi <dbtmdgns4296@naver.com>
2025-11-12 21:34:04 +0900

    refactor: 플러그인 추가 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,15 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  plugins: ["gatsby-plugin-typescript"],
+  plugins: [
+    "gatsby-plugin-typescript",
+    {
+      resolve: "gatsby-plugin-babel",
+      options: {
+        presets: [["@babel/preset-react", { runtime: "automatic" }]],
+      },
+    },
+  ],
 };
 
 export default config;
---
+++ gatsby-node.ts
@@ -0,0 +1,10 @@
+import type { GatsbyNode } from "gatsby";
+
+export const onCreateBabelConfig: GatsbyNode["onCreateBabelConfig"] = ({
+  actions,
+}) => {
+  actions.setBabelPreset({
+    name: "@babel/preset-react",
+    options: { runtime: "automatic" },
+  });
+};
